94b5d9654820622483f8c391f20f55fda4d1edb4
Debasish Pothal <debasish_pothal@hotmail.com>
2025-11-01 08:59:03 +0530

    Create 242.Valid-Anagram.js

---
+++ 242.Valid-Anagram.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  const map = new Map();
+
+  for (const c of s) {
+    map.set(c, (map.get(c) || 0) + 1);
+  }
+
+  for (const c of t) {
+    if (!map.has(c)) {
+      return false;
+    }
+
+    map.set(c, map.get(c) - 1);
+
+    if (map.get(c) === 0) {
+      map.delete(c);
+    }
+  }
+
+  return map.size === 0;
+};
