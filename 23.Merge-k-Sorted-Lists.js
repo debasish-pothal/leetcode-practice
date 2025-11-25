6c513d23d7811b1af8e6ec0accfde08be4955f65
Debasish Pothal <debasish_pothal@hotmail.com>
2025-11-25 19:11:38 +0530

    Create 23.Merge-k-Sorted-Lists.js

---
+++ 23.Merge-k-Sorted-Lists.js
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function (lists) {
+  lists = lists.filter((list) => list !== null);
+
+  if (lists.length === 0) {
+    return null;
+  }
+
+  const mergeTwoLists = (list1, list2) => {
+    const dummy = new ListNode(0);
+    let node = dummy;
+
+    while (list1 && list2) {
+      if (list1.val < list2.val) {
+        node.next = list1;
+        list1 = list1.next;
+      } else {
+        node.next = list2;
+        list2 = list2.next;
+      }
+
+      node = node.next;
+    }
+
+    node.next = list1 ? list1 : list2;
+
+    return dummy.next;
+  };
+
+  while (lists.length > 1) {
+    const mergedLists = [];
+
+    for (let i = 0; i < lists.length; i += 2) {
+      const list1 = lists[i];
+      const list2 = i + 1 < lists.length ? lists[i + 1] : null;
+
+      mergedLists.push(mergeTwoLists(list1, list2));
+    }
+
+    lists = mergedLists;
+  }
+
+  return lists[0];
+};
