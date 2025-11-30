6303337567ca5dfc7ee7707213f6a60a7c25673a
Debasish Pothal <debasish_pothal@hotmail.com>
2025-11-30 20:08:42 +0530

    Create 387.First-Unique-Character-in-a-String.js

---
+++ 387.First-Unique-Character-in-a-String.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function (s) {
+  const map = new Map();
+  let index = Infinity;
+
+  for (const c of s) {
+    map.set(c, (map.get(c) || 0) + 1);
+  }
+
+  for (const [key, value] of map) {
+    if (value === 1) {
+      index = Math.min(index, s.indexOf(key));
+    }
+  }
+
+  return index;
+};
