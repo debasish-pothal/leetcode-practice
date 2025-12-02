4d2587c39d20c9cbf15f1460b254b90bfe3d921f
Debasish Pothal <debasish_pothal@hotmail.com>
2025-12-02 11:09:49 +0530

    Create 119.Pascal-s-Triangle-II.js

---
+++ 119.Pascal-s-Triangle-II.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function (rowIndex) {
+  const result = Array(rowIndex + 1).fill(0);
+  result[0] = 1;
+
+  for (let i = 1; i <= rowIndex; i++) {
+    for (let j = i; j > 0; j--) {
+      result[j] += result[j - 1];
+    }
+  }
+
+  return result;
+};
