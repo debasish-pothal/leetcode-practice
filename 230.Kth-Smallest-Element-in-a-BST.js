47a8d11b742f011753c918418c8139e2633bc9f7
Debasish Pothal <debasish_pothal@hotmail.com>
2025-12-05 10:47:16 +0530

    Create 230.Kth-Smallest-Element-in-a-BST.js

---
+++ 230.Kth-Smallest-Element-in-a-BST.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function (root, k) {
+  let count = 0;
+  let result;
+
+  const traverse = (node) => {
+    if (!node) {
+      return;
+    }
+
+    traverse(node.left);
+    count += 1;
+
+    if (count === k) {
+      result = node.val;
+      return;
+    }
+
+    traverse(node.right);
+  };
+
+  traverse(root);
+
+  return result;
+};
