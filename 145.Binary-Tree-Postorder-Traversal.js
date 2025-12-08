4583085c8a5e5282d278f80c186dfe94447fa94c
Debasish Pothal <debasish_pothal@hotmail.com>
2025-12-08 21:09:14 +0530

    Create 145.Binary-Tree-Postorder-Traversal.js

---
+++ 145.Binary-Tree-Postorder-Traversal.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function (root) {
+  const result = [];
+
+  const traverse = (node) => {
+    if (node.left) traverse(node.left);
+    if (node.right) traverse(node.right);
+    result.push(node.val);
+  };
+
+  if (root) traverse(root);
+
+  return result;
+};
