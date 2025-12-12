834b3066d3b0d89a18a7b46b029cc615755e8568
Debasish Pothal <debasish_pothal@hotmail.com>
2025-12-12 16:46:02 +0530

    Update 3433.Count-Mentions-Per-User.js

--- 3433.Count-Mentions-Per-User.js
+++ 3433.Count-Mentions-Per-User.js
@@ -4,44 +4,54 @@
  * @return {number[]}
  */
 var countMentions = function (numberOfUsers, events) {
+  events.sort((a, b) => Number(a[1]) - Number(b[1]));
   const mentions = Array(numberOfUsers).fill(0);
-  const onlineUntil = Array(numberOfUsers).fill(0);
+  const offlineUntil = Array(numberOfUsers).fill(0);
 
-  const updateOnline = (time) => {
+  const updateOffline = (time) => {
     for (let i = 0; i < numberOfUsers; i++) {
-      if (onlineUntil[i] <= time) {
-        onlineUntil[i] = 0;
+      if (offlineUntil[i] !== 0 && offlineUntil[i] <= time) {
+        offlineUntil[i] = 0;
       }
     }
   };
 
-  for (const e of events) {
-    const [type, tString] = e;
-    const time = Number(tString);
+  let i = 0;
+  while (i < events.length) {
+    const time = Number(events[i][1]);
 
-    updateOnline(time);
+    // 1) Expire offline users
+    updateOffline(time);
 
-    if (type === "OFFLINE") {
-      const id = Number(e[2]);
-      onlineUntil[id] = time + 60;
-    } else {
-      const mentionStr = e[2];
-      const tokens = mentionStr.split(/\s+/);
+    // Gather all events at this timestamp
+    const same = [];
+    while (i < events.length && Number(events[i][1]) === time) {
+      same.push(events[i]);
+      i++;
+    }
 
-      for (const token of tokens) {
-        if (token === "ALL") {
-          for (let i = 0; i < numberOfUsers; i++) {
-            mentions[i]++;
-          }
-        } else if (token === "HERE") {
-          for (let i = 0; i < numberOfUsers; i++) {
-            if (onlineUntil[i] === 0) mentions[i]++;
-          }
-        } else if (token.startsWith("id")) {
-          const id = Number(token.slice(2));
+    // 2) Process all OFFLINE first
+    for (const e of same) {
+      if (e[0] === "OFFLINE") {
+        const id = Number(e[2]);
+        offlineUntil[id] = time + 60;
+      }
+    }
 
-          if (id >= 0 && id < numberOfUsers) {
-            mentions[id]++;
+    // 3) Then process all MESSAGE
+    for (const e of same) {
+      if (e[0] === "MESSAGE") {
+        const tokens = e[2].split(/\s+/);
+        for (const token of tokens) {
+          if (token === "ALL") {
+            for (let u = 0; u < numberOfUsers; u++) mentions[u]++;
+          } else if (token === "HERE") {
+            for (let u = 0; u < numberOfUsers; u++) {
+              if (offlineUntil[u] === 0) mentions[u]++;
+            }
+          } else if (token.startsWith("id")) {
+            const id = Number(token.slice(2));
+            if (id >= 0 && id < numberOfUsers) mentions[id]++;
           }
         }
       }
