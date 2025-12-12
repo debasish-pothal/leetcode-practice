ecbd33745f8485330664b6417d53873bc5dfdceb
Debasish Pothal <debasish_pothal@hotmail.com>
2025-12-12 15:34:57 +0530

    Create 11.Container-With-Most-Water.js

---
+++ 11.Container-With-Most-Water.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let area = 0;
+  let start = 0;
+  let end = height.length - 1;
+
+  while (start < end) {
+    const w = end - start;
+    const h = Math.min(height[start], height[end]);
+
+    area = Math.max(area, w * h);
+
+    if (height[start] < height[end]) {
+      start += 1;
+    } else {
+      end -= 1;
+    }
+  }
+
+  return area;
+};
