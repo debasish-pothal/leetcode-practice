ab0590e1ca98b865fcffa6d0edc43355148a7f3f
Debasish Pothal <debasish_pothal@hotmail.com>
2025-12-14 07:38:28 +0530

    Update 1436.Destination-City.js

--- 1436.Destination-City.js
+++ 1436.Destination-City.js
@@ -4,17 +4,14 @@
  */
 var destCity = function (paths) {
   const cities = new Set();
-  const pathMap = new Map();
 
   for (const [start, end] of paths) {
-    pathMap.set(start, end);
     cities.add(start);
-    cities.add(end);
   }
 
-  for (const city of cities) {
-    if (!pathMap.has(city)) {
-      return city;
+  for (const [start, end] of paths) {
+    if (!cities.has(end)) {
+      return end;
     }
   }
 };
