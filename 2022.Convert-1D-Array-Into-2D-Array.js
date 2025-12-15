f8da1255a28dc3f51d4efe510c038dbced191b58
Debasish Pothal <debasish_pothal@hotmail.com>
2025-12-15 20:08:50 +0530

    Create 2022.Convert-1D-Array-Into-2D-Array.js

---
+++ 2022.Convert-1D-Array-Into-2D-Array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} original
+ * @param {number} m
+ * @param {number} n
+ * @return {number[][]}
+ */
+var construct2DArray = function (original, m, n) {
+  if (m * n !== original.length) return [];
+
+  const result = [];
+
+  for (let i = 0; i < m; i++) {
+    result.push(original.slice(i * n, (i + 1) * n));
+  }
+
+  return result;
+};
