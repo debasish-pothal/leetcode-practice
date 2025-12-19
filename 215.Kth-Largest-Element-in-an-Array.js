b4e7f646b9e6cdb566f8943f392f772d22425c50
Debasish Pothal <debasish_pothal@hotmail.com>
2025-12-19 07:16:22 +0530

    Create 215.Kth-Largest-Element-in-an-Array.js

---
+++ 215.Kth-Largest-Element-in-an-Array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function (nums, k) {
+  const minHeap = new MinPriorityQueue();
+
+  for (const num of nums) {
+    minHeap.enqueue(num);
+
+    if (minHeap.size() > k) minHeap.dequeue();
+  }
+
+  return minHeap.dequeue();
+};
