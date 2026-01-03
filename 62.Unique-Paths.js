a94e635e746a21e91abfc0b8adf8362e7a8abd27
Debasish Pothal <debasish_pothal@hotmail.com>
2026-01-03 13:30:16 +0530

    Update 62.Unique-Paths.js

--- 62.Unique-Paths.js
+++ 62.Unique-Paths.js
@@ -4,15 +4,13 @@
  * @return {number}
  */
 var uniquePaths = function (m, n) {
-  const grid = Array.from({ length: m }, (_, i) =>
-    Array.from({ length: n }, (_, j) => (i === 0 || j === 0 ? 1 : undefined))
-  );
+  const grid = Array(n).fill(1);
 
   for (let i = 1; i < m; i++) {
     for (let j = 1; j < n; j++) {
-      grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+      grid[j] = grid[j] + grid[j - 1];
     }
   }
 
-  return grid[m - 1][n - 1];
+  return grid[n - 1];
 };
