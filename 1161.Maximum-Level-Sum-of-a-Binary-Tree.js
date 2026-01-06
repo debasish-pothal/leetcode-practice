aa90d3e87bded5cf6f760bee482ed31df47d9b54
Debasish Pothal <debasish_pothal@hotmail.com>
2026-01-06 18:24:49 +0530

    Create 1161.Maximum-Level-Sum-of-a-Binary-Tree.js

---
+++ 1161.Maximum-Level-Sum-of-a-Binary-Tree.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxLevelSum = function (root) {
+  if (!root) return 0;
+
+  let maxSum = root.val;
+  let currLevel = 1;
+  let maxLevel = 1;
+  const queue = [root];
+
+  while (queue.length) {
+    let currSum = 0;
+    const size = queue.length;
+
+    for (let i = 0; i < size; i++) {
+      const node = queue.shift();
+
+      currSum += node.val;
+
+      if (node.left) {
+        queue.push(node.left);
+      }
+
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+
+    if (currSum > maxSum) {
+      maxSum = currSum;
+      maxLevel = currLevel;
+    }
+
+    currLevel += 1;
+  }
+
+  return maxLevel;
+};
