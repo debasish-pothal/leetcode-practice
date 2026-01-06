1a06849c166e0ce19419186728436fb5c05d1f46
Debasish Pothal <debasish_pothal@hotmail.com>
2026-01-06 18:46:58 +0530

    Update 2363.Merge-Similar-Items.js

--- 2363.Merge-Similar-Items.js
+++ 2363.Merge-Similar-Items.js
@@ -4,36 +4,15 @@
  * @return {number[][]}
  */
 var mergeSimilarItems = function (items1, items2) {
-  items1.sort((a, b) => a[0] - b[0]);
-  items2.sort((a, b) => a[0] - b[0]);
+  const map = new Map();
 
-  const result = [];
-  let p1 = 0;
-  let p2 = 0;
-
-  while (p1 < items1.length && p2 < items2.length) {
-    if (items1[p1][0] < items2[p2][0]) {
-      result.push(items1[p1]);
-      p1 += 1;
-    } else if (items1[p1][0] > items2[p2][0]) {
-      result.push(items2[p2]);
-      p2 += 1;
-    } else {
-      result.push([items1[p1][0], items1[p1][1] + items2[p2][1]]);
-      p1 += 1;
-      p2 += 1;
-    }
+  for (const [key, val] of items1) {
+    map.set(key, (map.get(key) || 0) + val);
   }
 
-  while (p1 < items1.length) {
-    result.push(items1[p1]);
-    p1 += 1;
+  for (const [key, val] of items2) {
+    map.set(key, (map.get(key) || 0) + val);
   }
 
-  while (p2 < items2.length) {
-    result.push(items2[p2]);
-    p2 += 1;
-  }
-
-  return result;
+  return [...map.entries()].sort((a, b) => a[0] - b[0]);
 };
