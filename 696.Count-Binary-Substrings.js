72060b83b73f73c43914eda8c6d39c7ba8be9923
Debasish Pothal <debasish_pothal@hotmail.com>
2026-02-19 17:25:05 +0530

    Update 696.Count-Binary-Substrings.js

--- 696.Count-Binary-Substrings.js
+++ 696.Count-Binary-Substrings.js
@@ -4,23 +4,20 @@
  */
 var countBinarySubstrings = function (s) {
   let result = 0;
-  const groups = [];
-  let count = 1;
+  let prev_count = 0;
+  let curr_count = 1;
 
   for (let i = 1; i < s.length; i++) {
     if (s[i] === s[i - 1]) {
-      count += 1;
+      curr_count += 1;
     } else {
-      groups.push(count);
-      count = 1;
+      result += Math.min(prev_count, curr_count);
+      prev_count = curr_count;
+      curr_count = 1;
     }
   }
 
-  groups.push(count);
-
-  for (let i = 1; i < groups.length; i++) {
-    result += Math.min(groups[i - 1], groups[i]);
-  }
+  result += Math.min(prev_count, curr_count);
 
   return result;
 };
