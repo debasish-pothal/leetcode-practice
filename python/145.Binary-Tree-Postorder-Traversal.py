c751e04e8a5ebab12792215dcf58ef2be0290bd3
Debasish Pothal <debasish_pothal@hotmail.com>
2026-03-01 12:15:11 +0530

    Update 145.Binary-Tree-Postorder-Traversal.py

--- python/145.Binary-Tree-Postorder-Traversal.py
+++ python/145.Binary-Tree-Postorder-Traversal.py
@@ -6,15 +6,23 @@
 #         self.right = right
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+
         result = []
+        stack1 = [root]
+        stack2 = []
 
-        def traverse(node):
-            if not node:
-                return
-            traverse(node.left)
-            traverse(node.right)
-            result.append(node.val)
+        while len(stack1):
+            node = stack1.pop()
+            stack2.append(node)
 
-        traverse(root)
+            if node.left:
+                stack1.append(node.left)
+            if node.right:
+                stack1.append(node.right)
+
+        while len(stack2):
+            result.append(stack2.pop().val)
 
         return result
