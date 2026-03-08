515768024f23e4905923d3aa688bc7f8a526efee
Debasish Pothal <debasish_pothal@hotmail.com>
2026-03-08 07:22:44 +0530

    Update 389.Find-the-Difference.py

--- python/389.Find-the-Difference.py
+++ python/389.Find-the-Difference.py
@@ -1,18 +1,8 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        tMap = {}
+        res = 0
 
-        for c in t:
-            if c not in tMap:
-                tMap[c] = 0
-            tMap[c] += 1
+        for c in s + t:
+            res ^= ord(c)
 
-        for c in s:
-            tMap[c] -= 1
-            if tMap[c] == 0:
-                del tMap[c]
-
-        for key in tMap:
-            return key
-
-        return ""
+        return chr(res)
