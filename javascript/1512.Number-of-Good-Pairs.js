c54a8e00c79b1457ae440903b5f2e772d68d6df0
Debasish Pothal <debasish_pothal@hotmail.com>
2026-03-08 19:50:13 +0530

    Update 1512.Number-of-Good-Pairs.js

--- javascript/1512.Number-of-Good-Pairs.js
+++ javascript/1512.Number-of-Good-Pairs.js
@@ -6,11 +6,11 @@ var numIdenticalPairs = function (nums) {
   const map = new Map();
   let count = 0;
 
-  for (let i = 0; i < nums.length; i++) {
-    map.set(nums[i], (map.get(nums[i]) || 0) + 1);
+  for (const num of nums) {
+    map.set(num, (map.get(num) || 0) + 1);
   }
 
-  for (const [key, val] of map) {
+  for (const val of map.values()) {
     if (val > 1) {
       count += (val * (val - 1)) / 2;
     }
