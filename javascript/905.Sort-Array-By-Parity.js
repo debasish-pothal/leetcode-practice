b60db2b55328d618871210881a518bbd30f2ceb5
Debasish Pothal <debasish_pothal@hotmail.com>
2026-03-08 07:33:12 +0530

    Update 905.Sort-Array-By-Parity.js

--- javascript/905.Sort-Array-By-Parity.js
+++ javascript/905.Sort-Array-By-Parity.js
@@ -3,8 +3,20 @@
  * @return {number[]}
  */
 var sortArrayByParity = function (nums) {
-  even = nums.filter((n) => n % 2 === 0);
-  odd = nums.filter((n) => n % 2 !== 0);
+  even = 0;
+  odd = nums.length - 1;
 
-  return [...even, ...odd];
+  while (even < odd) {
+    if (nums[even] % 2 === 0) {
+      even += 1;
+    } else if (nums[odd] % 2 !== 0) {
+      odd -= 1;
+    } else {
+      [nums[even], nums[odd]] = [nums[odd], nums[even]];
+      even += 1;
+      odd -= 1;
+    }
+  }
+
+  return nums;
 };
