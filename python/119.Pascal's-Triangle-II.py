f6c07b627b0bec877d56df0c7daf91602c05e840
Debasish Pothal <debasish_pothal@hotmail.com>
2026-03-09 21:19:25 +0530

    Update 119.Pascal's-Triangle-II.py

--- python/119.Pascal's-Triangle-II.py
+++ python/119.Pascal's-Triangle-II.py
@@ -1,10 +1,12 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        result = [0] * (rowIndex + 1)
-        result[0] = 1
+        res = [1]
 
-        for i in range(1, rowIndex + 1):
-            for j in range(i, 0, -1):
-                result[j] += result[j - 1]
+        for i in range(rowIndex):
+            next_row = [0] * (len(res) + 1)
+            for j in range(len(res)):
+                next_row[j] += res[j]
+                next_row[j + 1] += res[j]
+            res = next_row
 
-        return result
+        return res
