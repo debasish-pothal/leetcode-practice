6fb8597c35a5185219f4825ef4b4e3acd355790e
Debasish Pothal <debasish_pothal@hotmail.com>
2026-03-09 21:05:36 +0530

    Create 119.Pascal's-Triangle-II.py

---
+++ python/119.Pascal's-Triangle-II.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        result = [0] * (rowIndex + 1)
+        result[0] = 1
+
+        for i in range(1, rowIndex + 1):
+            for j in range(i, 0, -1):
+                result[j] += result[j - 1]
+
+        return result
