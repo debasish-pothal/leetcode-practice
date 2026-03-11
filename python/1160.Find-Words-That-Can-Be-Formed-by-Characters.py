3a37ecbd372b9071f3cad6ded7b7413b74685ac4
Debasish Pothal <debasish_pothal@hotmail.com>
2026-03-11 13:44:08 +0530

    Update 1160.Find-Words-That-Can-Be-Formed-by-Characters.py

--- python/1160.Find-Words-That-Can-Be-Formed-by-Characters.py
+++ python/1160.Find-Words-That-Can-Be-Formed-by-Characters.py
@@ -1,18 +1,13 @@
 from collections import Counter
 
 class Solution:
-    def hasAllChars(self, charsMap, wordMap) -> bool:
-        for key, val in wordMap.items():
-            if key not in charsMap or charsMap.get(key) < val:
-                return False
-        return True
-
     def countCharacters(self, words: List[str], chars: str) -> int:
-        charsMap = Counter(chars)
+        chars_count = Counter(chars)
         length = 0
         
         for word in words:
-            if (self.hasAllChars(charsMap, Counter(word))):
+            word_count = Counter(word)
+            if all(chars_count[c] >= word_count[c] for c in word_count):
                 length += len(word)
 
         return length
