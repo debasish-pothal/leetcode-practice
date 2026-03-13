f943f94e0c1784ed16b816123f51f199196b8b86
Debasish Pothal <debasish_pothal@hotmail.com>
2026-03-13 11:30:30 +0530

    Update 1913.Maximum-Product-Difference-Between-Two-Pairs.js

--- javascript/1913.Maximum-Product-Difference-Between-Two-Pairs.js
+++ javascript/1913.Maximum-Product-Difference-Between-Two-Pairs.js
@@ -3,12 +3,26 @@
  * @return {number}
  */
 var maxProductDifference = function (nums) {
-  nums.sort((a, b) => a - b);
+  let min1 = Infinity;
+  let min2 = Infinity;
+  let max1 = -Infinity;
+  let max2 = -Infinity;
 
-  const min1 = nums[0];
-  const min2 = nums[1];
-  const max1 = nums[nums.length - 1];
-  const max2 = nums[nums.length - 2];
+  for (const num of nums) {
+    if (num < min1) {
+      min2 = min1;
+      min1 = num;
+    } else if (num < min2) {
+      min2 = num;
+    }
+
+    if (num > max1) {
+      max2 = max1;
+      max1 = num;
+    } else if (num > max2) {
+      max2 = num;
+    }
+  }
 
   return max1 * max2 - min1 * min2;
 };
