31f9103c1b1588671c0617b663bf7f60a370163c
Debasish Pothal <debasish_pothal@hotmail.com>
2026-03-14 12:11:46 +0530

    Create 2706.Buy-Two-Chocolates.py

---
+++ python/2706.Buy-Two-Chocolates.py
@@ -0,0 +1,14 @@
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        min1, min2 = float('inf'), float('inf')
+
+        for price in prices:
+            if price < min1:
+                min2 = min1
+                min1 = price
+            elif price < min2:
+                min2 = price
+
+        left = money - (min1 + min2)
+
+        return money if left < 0 else left
