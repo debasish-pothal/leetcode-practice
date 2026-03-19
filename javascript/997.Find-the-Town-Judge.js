8a20223c71d0d92278293d4126c096dfda5dde98
Debasish Pothal <debasish_pothal@hotmail.com>
2026-03-19 16:44:07 +0530

    Update 997.Find-the-Town-Judge.js

--- javascript/997.Find-the-Town-Judge.js
+++ javascript/997.Find-the-Town-Judge.js
@@ -4,27 +4,16 @@
  * @return {number}
  */
 var findJudge = function (n, trust) {
-  const incomingMap = new Map();
-  const outgoingMap = new Map();
-  const incomingEdges = n - 1;
-  const outgoingEdges = 0;
-
-  for (let i = 1; i <= n; i++) {
-    incomingMap.set(i, 0);
-    outgoingMap.set(i, 0);
-  }
+  const trustArr = Array(n).fill(0);
 
   for (const [j1, j2] of trust) {
-    incomingMap.set(j2, incomingMap.get(j2) + 1);
-    outgoingMap.set(j1, outgoingMap.get(j1) + 1);
+    trustArr[j1 - 1] -= 1;
+    trustArr[j2 - 1] += 1;
   }
 
-  for (let i = 1; i <= n; i++) {
-    if (
-      incomingMap.get(i) === incomingEdges &&
-      outgoingMap.get(i) === outgoingEdges
-    ) {
-      return i;
+  for (let i = 0; i < n; i++) {
+    if (trustArr[i] === n - 1) {
+      return i + 1;
     }
   }
 
