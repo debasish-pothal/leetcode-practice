f41d1eb1395b5a7b74b0330df2d852feb4eaab48
Debasish Pothal <debasish_pothal@hotmail.com>
2026-03-21 16:52:51 +0530

    Update 349.Intersection-of-Two-Arrays.py

--- python/349.Intersection-of-Two-Arrays.py
+++ python/349.Intersection-of-Two-Arrays.py
@@ -2,10 +2,10 @@ class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         res = []
         set1 = set(nums1)
-        set2 = set(nums2)
 
-        for num in set2:
+        for num in nums2:
             if num in set1:
                 res.append(num)
+                set1.remove(num)
 
         return res
