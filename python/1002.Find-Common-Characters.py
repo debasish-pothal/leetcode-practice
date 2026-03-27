825fc6f39f6706c814565782816d4260bc7cfca1
Debasish Pothal <debasish_pothal@hotmail.com>
2026-03-27 21:58:57 +0530

    Update 1002.Find-Common-Characters.py

--- python/1002.Find-Common-Characters.py
+++ python/1002.Find-Common-Characters.py
@@ -1,17 +1,9 @@
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
-        def createMap(word):
-            freq = {}
-
-            for c in word:
-                freq[c] = freq.get(c, 0) + 1
-
-            return freq
-
-        curr_freq = createMap(words[0])
+        curr_freq = Counter(words[0])
 
         for i in range(1, len(words)):
-            temp_freq = createMap(words[i])
+            temp_freq = Counter(words[i])
 
             for key in list(curr_freq.keys()):
                 val = temp_freq.get(key, 0)
