9a7542a60752d3bc3e9e0ff193d99514ac73c1d3
Debasish Pothal <debasish_pothal@hotmail.com>
2026-03-27 22:00:39 +0530

    Update 1002.Find-Common-Characters.py

--- python/1002.Find-Common-Characters.py
+++ python/1002.Find-Common-Characters.py
@@ -3,15 +3,7 @@ class Solution:
         curr_freq = Counter(words[0])
 
         for i in range(1, len(words)):
-            temp_freq = Counter(words[i])
-
-            for key in list(curr_freq.keys()):
-                val = temp_freq.get(key, 0)
-
-                if val == 0:
-                    del curr_freq[key]
-                else:
-                    curr_freq[key] = min(curr_freq[key], val)
+            curr_freq &= Counter(words[i])
 
         result = []
 
