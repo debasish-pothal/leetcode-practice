8d291c2526c8102868bf7b9ac05dfe99374a6fcd
Debasish Pothal <debasish_pothal@hotmail.com>
2026-03-29 13:34:56 +0530

    Update 1380.Lucky-Numbers-in-a-Matrix.js

--- javascript/1380.Lucky-Numbers-in-a-Matrix.js
+++ javascript/1380.Lucky-Numbers-in-a-Matrix.js
@@ -3,22 +3,30 @@
  * @return {number[]}
  */
 var luckyNumbers = function (matrix) {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
   const lucky = [];
 
-  const isLucky = (num, row, col) => {
-    const min = Math.min(...matrix[row]);
+  const rows_min = [];
+  const cols_max = [];
+
+  for (let r = 0; r < rows; r++) {
+    rows_min.push(Math.min(...matrix[r]));
+  }
+
+  for (let c = 0; c < cols; c++) {
     let max = -Infinity;
 
-    for (let r = 0; r < matrix.length; r++) {
-      max = Math.max(max, matrix[r][col]);
+    for (let r = 0; r < rows; r++) {
+      max = Math.max(max, matrix[r][c]);
     }
 
-    return num === min && num === max;
-  };
+    cols_max.push(max);
+  }
 
-  for (let r = 0; r < matrix.length; r++) {
-    for (let c = 0; c < matrix[0].length; c++) {
-      if (isLucky(matrix[r][c], r, c)) {
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < cols; c++) {
+      if (matrix[r][c] === rows_min[r] && matrix[r][c] === cols_max[c]) {
         lucky.push(matrix[r][c]);
       }
     }
