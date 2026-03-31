f0b338622586f1f5b90d177c3200498e94fe5380
Debasish Pothal <debasish_pothal@hotmail.com>
2026-03-31 17:15:26 +0530

    Update 1460.Make-Two-Arrays-Equal-by-Reversing-Subarrays.py

--- python/1460.Make-Two-Arrays-Equal-by-Reversing-Subarrays.py
+++ python/1460.Make-Two-Arrays-Equal-by-Reversing-Subarrays.py
@@ -1,3 +1,13 @@
 class Solution:
     def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
-        return sorted(target) == sorted(arr)
+        freq = defaultdict(int)
+
+        for n1, n2 in zip(target, arr):
+            freq[n1] += 1
+            freq[n2] -= 1
+
+        for n in freq:
+            if freq[n] != 0:
+                return False
+
+        return True
