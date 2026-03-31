806aeb3f3134eef966aabe6b7a80def490f26787
Debasish Pothal <debasish_pothal@hotmail.com>
2026-03-31 06:40:16 +0530

    Update 2678.Number-of-Senior-Citizens.py

--- python/2678.Number-of-Senior-Citizens.py
+++ python/2678.Number-of-Senior-Citizens.py
@@ -1,10 +1,3 @@
 class Solution:
     def countSeniors(self, details: List[str]) -> int:
-        count = 0
-
-        for detail in details:
-            age = int(detail[11:13])
-            if age > 60:
-                count += 1
-        
-        return count
+        return sum(int(detail[11:13]) > 60 for detail in details)
