172bf45d451041eff181b4e2fd6ac984141c0f71
Debasish Pothal <debasish_pothal@hotmail.com>
2026-04-02 06:27:46 +0530

    Create 2220.Minimum-Bit-Flips-to-Convert-Number.js

---
+++ javascript/2220.Minimum-Bit-Flips-to-Convert-Number.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} start
+ * @param {number} goal
+ * @return {number}
+ */
+var minBitFlips = function (start, goal) {
+  let count = 0;
+
+  while (start || goal) {
+    if (start % 2 !== goal % 2) {
+      count += 1;
+    }
+
+    start = Math.floor(start / 2);
+    goal = Math.floor(goal / 2);
+  }
+
+  return count;
+};
