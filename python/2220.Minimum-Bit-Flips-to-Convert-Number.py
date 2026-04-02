c2abfd6f1b71fc7862bfa9e838740050ac8fa484
Debasish Pothal <debasish_pothal@hotmail.com>
2026-04-02 06:27:50 +0530

    Create 2220.Minimum-Bit-Flips-to-Convert-Number.py

---
+++ python/2220.Minimum-Bit-Flips-to-Convert-Number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        count = 0
+
+        while start or goal:
+            if start % 2 != goal % 2:
+                count += 1
+            start //= 2
+            goal //= 2
+
+        return count
