75e4a1a6da21173521bce8edf884ba7eaf3c4463
Debasish Pothal <debasish_pothal@hotmail.com>
2026-04-02 06:50:38 +0530

    Create 222.Count-Complete-Tree-Nodes.js

---
+++ javascript/222.Count-Complete-Tree-Nodes.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countNodes = function (root) {
+  let count = 0;
+
+  if (!root) return count;
+
+  const dfs = (node) => {
+    count += 1;
+
+    if (node.left) dfs(node.left);
+    if (node.right) dfs(node.right);
+  };
+
+  dfs(root);
+
+  return count;
+};
