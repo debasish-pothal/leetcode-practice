a66d6be69d439fa7faa1e6bf0406d7775eba9cb0
Debasish Pothal <debasish_pothal@hotmail.com>
2026-04-09 07:19:17 +0530

    Create 1752.Check-if-Array-Is-Sorted-and-Rotated.js

---
+++ javascript/1752.Check-if-Array-Is-Sorted-and-Rotated.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var check = function (nums) {
+  const n = nums.length;
+  const copy = [...nums, ...nums];
+
+  let start = 0;
+
+  while (start < n - 1) {
+    if (copy[start] <= copy[start + 1]) {
+      start += 1;
+    } else {
+      break;
+    }
+  }
+
+  if (start === n - 1) {
+    return true;
+  }
+
+  for (let i = start + 1; i < start + n; i++) {
+    if (copy[i] > copy[i + 1]) {
+      return false;
+    }
+  }
+
+  return true;
+};
