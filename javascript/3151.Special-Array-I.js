edb019288d7c77b38bddb1798eea0fa28fed135e
Debasish Pothal <debasish_pothal@hotmail.com>
2026-04-09 06:27:53 +0530

    Create 3151.Special-Array-I.js

---
+++ javascript/3151.Special-Array-I.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isArraySpecial = function (nums) {
+  const n = nums.length;
+
+  if (n === 1) {
+    return true;
+  }
+
+  for (let i = 0; i < n - 1; i++) {
+    if (nums[i] % 2 === nums[i + 1] % 2) {
+      return false;
+    }
+  }
+
+  return true;
+};
