be19a75f4c0df0640b6a8b14ca50209854ec863d
Debasish Pothal <debasish_pothal@hotmail.com>
2026-04-13 06:27:01 +0530

    Update 1534.Count-Good-Triplets.js

--- javascript/1534.Count-Good-Triplets.js
+++ javascript/1534.Count-Good-Triplets.js
@@ -6,16 +6,17 @@
  * @return {number}
  */
 var countGoodTriplets = function (arr, a, b, c) {
+  const n = arr.length;
   let count = 0;
 
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      for (let k = j + 1; k < arr.length; k++) {
-        if (
-          Math.abs(arr[i] - arr[j]) <= a &&
-          Math.abs(arr[j] - arr[k]) <= b &&
-          Math.abs(arr[i] - arr[k]) <= c
-        ) {
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      if (Math.abs(arr[i] - arr[j]) > a) {
+        continue;
+      }
+
+      for (let k = j + 1; k < n; k++) {
+        if (Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k]) <= c) {
           count += 1;
         }
       }
