0a8446ed0c3a7062eacedd9ecd1a309de474886b
Debasish Pothal <debasish_pothal@hotmail.com>
2026-04-13 21:20:08 +0530

    Update 2873.Maximum-Value-of-an-Ordered-Triplet-I.js

--- javascript/2873.Maximum-Value-of-an-Ordered-Triplet-I.js
+++ javascript/2873.Maximum-Value-of-an-Ordered-Triplet-I.js
@@ -5,13 +5,14 @@
 var maximumTripletValue = function (nums) {
   let result = 0;
   const n = nums.length;
+  let left = nums[0];
 
-  for (let i = 0; i < n; i++) {
-    for (let j = i + 1; j < n; j++) {
-      for (let k = j + 1; k < n; k++) {
-        result = Math.max(result, (nums[i] - nums[j]) * nums[k]);
-      }
+  for (let j = 1; j < n; j++) {
+    for (let k = j + 1; k < n; k++) {
+      result = Math.max(result, (left - nums[j]) * nums[k]);
     }
+
+    left = Math.max(left, nums[j]);
   }
 
   return result;
