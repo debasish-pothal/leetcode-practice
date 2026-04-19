845d1dea74f23cbe73e601fe4516c0d6b82c0131
Debasish Pothal <debasish_pothal@hotmail.com>
2026-04-19 20:25:27 +0530

    Update 75.Sort-Colors.js

--- javascript/75.Sort-Colors.js
+++ javascript/75.Sort-Colors.js
@@ -3,28 +3,20 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function (nums) {
-  const n = nums.length;
-  // move 0 to start of array
-  let index = n - 1;
-  let zeroIndex = n - 1;
+  let low = 0;
+  let mid = 0;
+  let high = nums.length - 1;
 
-  while (index >= 0) {
-    if (nums[index] !== 0) {
-      [nums[index], nums[zeroIndex]] = [nums[zeroIndex], nums[index]];
-      zeroIndex -= 1;
+  while (mid <= high) {
+    if (nums[mid] === 0) {
+      [nums[low], nums[mid]] = [nums[mid], nums[low]];
+      low += 1;
+      mid += 1;
+    } else if (nums[mid] === 1) {
+      mid += 1;
+    } else {
+      [nums[mid], nums[high]] = [nums[high], nums[mid]];
+      high -= 1;
     }
-    index -= 1;
-  }
-
-  // move 2 to end of array
-  index = zeroIndex + 1;
-  let twoIndex = zeroIndex + 1;
-
-  while (index < n) {
-    if (nums[index] !== 2) {
-      [nums[index], nums[twoIndex]] = [nums[twoIndex], nums[index]];
-      twoIndex += 1;
-    }
-    index += 1;
   }
 };
