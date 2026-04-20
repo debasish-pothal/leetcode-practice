3a42ca5edc5f85cb8e6677cad97b117cdaa28f13
Debasish Pothal <debasish_pothal@hotmail.com>
2026-04-20 20:47:19 +0530

    Update 3783.Mirror-Distance-of-an-Integer.py

--- python/3783.Mirror-Distance-of-an-Integer.py
+++ python/3783.Mirror-Distance-of-an-Integer.py
@@ -1,6 +1,11 @@
 class Solution:
     def mirrorDistance(self, n: int) -> int:
-        def reverse(n):
-            return int(str(n)[::-1])
+        rev_num = 0
+        n_copy = n
 
-        return abs(n - reverse(n))
+        while n_copy:
+            digit = n_copy % 10
+            rev_num = rev_num * 10 + digit
+            n_copy //= 10 
+
+        return abs(n - rev_num)
