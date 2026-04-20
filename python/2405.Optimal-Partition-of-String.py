4a7caec58a458dd1847d9639af358c3b9b23098b
Debasish Pothal <debasish_pothal@hotmail.com>
2026-04-20 20:37:00 +0530

    Create 2405.Optimal-Partition-of-String.py

---
+++ python/2405.Optimal-Partition-of-String.py
@@ -0,0 +1,12 @@
+class Solution:
+    def partitionString(self, s: str) -> int:
+        seen = set()
+        count = 1
+
+        for c in s:
+            if c in seen:
+                count += 1
+                seen = set()
+            seen.add(c)
+
+        return count
