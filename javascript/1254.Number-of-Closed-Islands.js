0884567c5433b26d09f23521a1ffeb081c240412
Debasish Pothal <debasish_pothal@hotmail.com>
2026-04-22 06:48:27 +0530

    Create 1254.Number-of-Closed-Islands.js

---
+++ javascript/1254.Number-of-Closed-Islands.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var closedIsland = function (grid) {
+  const rows = grid.length;
+  const cols = grid[0].length;
+  const visited = new Set();
+  let count = 0;
+
+  const dfs = (r, c) => {
+    if (r < 0 || c < 0 || r >= rows || c >= cols) {
+      return 0;
+    }
+
+    if (grid[r][c] === 1) {
+      return 1;
+    }
+
+    if (visited.has(`${r}-${c}`)) {
+      return 1;
+    }
+
+    visited.add(`${r}-${c}`);
+
+    return Math.min(dfs(r + 1, c), dfs(r - 1, c), dfs(r, c + 1), dfs(r, c - 1));
+  };
+
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < cols; c++) {
+      if (grid[r][c] === 0 && !visited.has(`${r}-${c}`)) {
+        count += dfs(r, c);
+      }
+    }
+  }
+
+  return count;
+};
