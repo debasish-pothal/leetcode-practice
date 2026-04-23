346360c87e55b4c10f00fe908635bcfce502108c
Debasish Pothal <debasish_pothal@hotmail.com>
2026-04-23 20:10:02 +0530

    Update 2348.Number-of-Zero-Filled-Subarrays.js

--- javascript/2348.Number-of-Zero-Filled-Subarrays.js
+++ javascript/2348.Number-of-Zero-Filled-Subarrays.js
@@ -3,27 +3,15 @@
  * @return {number}
  */
 var zeroFilledSubarray = function (nums) {
-  const factorial = (n) => (n * (n + 1)) / 2;
-
-  const countConsecutiveZeros = (idx) => {
-    let length = 0;
-
-    while (nums[idx] === 0) {
-      length += 1;
-      idx += 1;
-    }
-
-    return [length, idx];
-  };
-
   let result = 0;
+  let zeros = 0;
 
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] === 0) {
-      const [length, idx] = countConsecutiveZeros(i);
-
-      result += factorial(length);
-      i = idx;
+      zeros += 1;
+      result += zeros;
+    } else {
+      zeros = 0;
     }
   }
 
