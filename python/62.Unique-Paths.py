5ba7cafb1905b32cfc25e47dac6627dc51b295bb
Debasish Pothal <debasish_pothal@hotmail.com>
2026-05-08 12:12:58 +0530

    Update 62.Unique-Paths.py

--- python/62.Unique-Paths.py
+++ python/62.Unique-Paths.py
@@ -1,16 +1,9 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        result = [[0] * n for _ in range(m)]
-
-        for r in range(m):
-            result[r][0] = 1
-
-        for c in range(n):
-            result[0][c] = 1
+        result = [1] * n
 
         for r in range(1, m):
             for c in range(1, n):
-                result[r][c] = result[r - 1][c] + result[r][c - 1]
+                result[c] = result[c] + result[c - 1]
 
-
-        return result[m - 1][n - 1]
+        return result[n - 1]
