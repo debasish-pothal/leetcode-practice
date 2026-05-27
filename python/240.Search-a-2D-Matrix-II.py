74914245782bc04297475d056f51bb1656287790
Debasish Pothal <debasish_pothal@hotmail.com>
2026-05-27 20:37:34 +0530

    Create 240.Search-a-2D-Matrix-II.py

---
+++ python/240.Search-a-2D-Matrix-II.py
@@ -0,0 +1,17 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        row = 0
+        col = cols - 1
+
+        while row < rows and col >= 0:
+            if matrix[row][col] > target:
+                col -= 1
+            elif matrix[row][col] < target:
+                row += 1
+            else:
+                return True
+        
+        return False
