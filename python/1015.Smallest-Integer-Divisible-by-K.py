c744bb8a47077331ca3592ea799e1a8499f2d2f2
Debasish Pothal <debasish_pothal@hotmail.com>
2026-06-02 19:56:05 +0530

    Create 1015.Smallest-Integer-Divisible-by-K.py

---
+++ python/1015.Smallest-Integer-Divisible-by-K.py
@@ -0,0 +1,15 @@
+class Solution:
+    def smallestRepunitDivByK(self, k: int) -> int:
+        if k % 2 == 0 or k % 5 == 0:
+            return -1
+
+        length = 0
+        remainder = 0
+
+        while True:
+            length += 1
+
+            remainder = remainder * 10 + 1
+
+            if remainder % k == 0:
+                return length
